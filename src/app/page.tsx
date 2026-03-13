3e5ad3625a154dcc68931f5d209242babec5a47a
Obhahie Praise <obhahiepraise@gmail.com>
2026-03-13 21:48:18 +0100

    feat: implement initial waitlist application with form, UI, and success modal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,87 +45,81 @@ const WaitlistContent = () => {
     <div className="pt-6 relative min-h-screen bg-background text-foreground overflow-x-hidden transition-colors duration-300">
       <WaitlistHeader />
       <main className="relative">
-        <div className="space-y-6 pt-12">
+        <div className="space-y-12 md:space-y-6 pt-12 grid grid-cols-1 md:grid-cols-2 px-6 md:px-20">
           <div className="">
-            <div className="flex gap-2 items-center font-medium text-xs backdrop-blur-lg py-1 px-3 border border-zinc-200 dark:border-zinc-800/30 rounded-lg w-fit mx-auto relative bg-zinc-50/50 dark:bg-transparent shadow-sm dark:shadow-none">
-              <Twitter strokeWidth={1.6} size={18} className="text-zinc-600 dark:text-zinc-400" />{" "}
-              <p className="text-zinc-600 dark:text-zinc-400">Check us out on X</p>
+            <h1 className="text-left text-5xl lg:text-[4.5rem] font-medium capitalize leading-none tracking-tight text-zinc-900 dark:text-white">
+              One tap. 
+              <span className="special-font inline-flex items-center bg-linear-to-r from-blue-500/60 to-red-500/60 bg-clip-text text-transparent relative">
+                <p>Instant Alert.</p>
+              </span>
+            Faster Help.
+            </h1>
+            <p className="text-left my-10 text-lg text-zinc-500 dark:text-neutral-500">
+              Unlock exclusive early access to a ground-breaking to respond to
+              emergencies! <br className="hidden md:block" />
+              Subscribe now and stay ahead of the future!
+            </p>
+            <div className="bg-white dark:bg-zinc-900/40 backdrop-blur-lg border border-zinc-200 dark:border-zinc-800/60 rounded-full w-full max-w-lg md:w-3/4 shadow-xl dark:shadow-none overflow-hidden">
+              <form onSubmit={handleJoin} className="flex">
+                <input
+                  type="email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  disabled={status === "loading"}
+                  className="px-6 py-3 bg-transparent text-zinc-900 dark:text-white rounded-full outline-none focus:outline-none disabled:opacity-50 w-full"
+                  placeholder="Your email"
+                />
+                <button
+                  type="submit"
+                  disabled={status === "loading"}
+                  className="flex items-center gap-2 text-nowrap bg-zinc-900 dark:bg-white text-white dark:text-black hover:bg-zinc-800 dark:hover:bg-zinc-200 transition-all cursor-pointer py-3 px-8 rounded-full font-medium disabled:opacity-50 shadow-lg dark:shadow-none active:scale-95"
+                >
+                  <p>{status === "loading" ? "Joining..." : "Join waitlist"}</p>
+                </button>
+              </form>
             </div>
-          </div>
-          <h1 className="text-center text-[5rem] font-medium leading-none text-zinc-900 dark:text-white">
-            Early Access to Game <br /> Changing{" "}
-            <span className="special-font inline-flex items-center bg-linear-to-r from-blue-500/60 to-red-500/60 bg-clip-text text-transparent relative">
-              <p>Emergency Responses</p>
-              <Ambulance
-                size={55}
-                className="text-zinc-900 dark:text-white ml-2"
-              />
-            </span>
-          </h1>
-          <p className="text-center text-lg text-zinc-500 dark:text-neutral-500 font-medium">
-            Unlock exclusive early access to a ground-breaking to respond to
-            emergencies! <br />
-            Subscribe now and stay ahead of the future!
-          </p>
-          <div className="bg-white dark:bg-zinc-900/40 backdrop-blur-lg border border-zinc-200 dark:border-zinc-800/60 rounded-full w-fit mx-auto shadow-xl dark:shadow-none overflow-hidden">
-            <form onSubmit={handleJoin} className="flex">
-              <input
-                type="email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                disabled={status === "loading"}
-                className="px-6 py-3 bg-transparent text-zinc-900 dark:text-white rounded-full outline-none focus:outline-none disabled:opacity-50"
-                size={30}
-                placeholder="Your email"
-              />
-              <button
-                type="submit"
-                disabled={status === "loading"}
-                className="flex items-center gap-2 bg-zinc-900 dark:bg-white text-white dark:text-black hover:bg-zinc-800 dark:hover:bg-zinc-200 transition-all cursor-pointer py-3 px-8 rounded-full font-medium disabled:opacity-50 shadow-lg dark:shadow-none active:scale-95"
-              >
-                <PlusCircle strokeWidth={1.7} size={20} />
-                <p>{status === "loading" ? "Joining..." : "Join"}</p>
-              </button>
-            </form>
-          </div>
-          {status === "error" && (
-            <p className="text-red-500 text-center text-sm font-medium">{errorMessage}</p>
-          )}
-          <div className="flex items-center justify-center my-5">
-            <div className="">
-              <div className="relative">
-                <div className="h-7 w-20 relative">
-                  <Image
-                    src="https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=1170&auto=format&fit=crop"
-                    alt="User 1"
-                    width={28}
-                    height={28}
-                    className="h-7 w-7 rounded-full border border-zinc-200 dark:border-zinc-800/60 absolute top-0 left-0 object-cover"
+            {status === "error" && (
+              <p className="text-red-500 text-center text-sm font-medium">
+                {errorMessage}
+              </p>
+            )}
+            <div className="flex items-center justify-start my-5">
+              <div className="">
+                <div className="relative">
+                  <img
+                    src="https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                    className="h-7 w-7 rounded-full border border-zinc-200 dark:border-zinc-800/60 absolute top-0 left-0"
                   />
-                  <Image
-                    src="https://images.unsplash.com/photo-1595211877493-41a4e5f236b3?q=80&w=715&auto=format&fit=crop"
-                    alt="User 2"
-                    width={28}
-                    height={28}
-                    className="h-7 w-7 rounded-full border border-zinc-200 dark:border-zinc-800/60 absolute top-0 left-4 object-cover"
+                  <img
+                    src="https://images.unsplash.com/photo-1595211877493-41a4e5f236b3?q=80&w=715&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                    className="h-7 w-7 rounded-full border border-zinc-200 dark:border-zinc-800/60 absolute top-0 left-4"
                   />
-                  <Image
-                    src="https://images.unsplash.com/photo-1580518324671-c2f0833a3af3?q=80&w=1974&auto=format&fit=crop"
-                    alt="User 3"
-                    width={28}
-                    height={28}
-                    className="h-7 w-7 rounded-full border border-zinc-200 dark:border-zinc-800/60 absolute top-0 left-8 object-cover"
+                  <img
+                    src="https://images.unsplash.com/photo-1580518324671-c2f0833a3af3?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                    className="h-7 w-7 rounded-full border border-zinc-200 dark:border-zinc-800/60 absolute top-0 left-8"
                   />
                 </div>
+                <p className="text-zinc-500 dark:text-white/40 text-sm my-2 ml-14">
+                  Join 500+ already onboard!
+                </p>
               </div>
-              <p className="text-zinc-500 dark:text-white/40 text-sm my-2 ml-18">
-                Join 500+ already onboard!
-              </p>
             </div>
           </div>
+          <div className="flex justify-center md:block">
+            <Image
+              src="/mobile-illustro.svg"
+              alt="whop-logo"
+              width={800}
+              height={800}
+              className="w-full max-w-md md:max-w-full"
+            />
+          </div>
         </div>
-        <div className="grid grid-cols-3 gap-8 mx-60 mt-40 mb-20 text-zinc-900 dark:text-white">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-16 md:gap-8 mx-10 md:mx-60 mt-24 md:mt-80 mb-20 text-zinc-900 dark:text-white">
           <div className="relative bg-zinc-50 dark:bg-neutral-700/20 backdrop-blur-2xl h-45 flex items-center justify-center border-zinc-200 dark:border-zinc-800/60 border rounded-2xl text-2xl text-center font-medium shadow-sm dark:shadow-none transition-transform hover:scale-[1.02]">
             <div className="text-white dark:text-black bg-zinc-900 dark:bg-zinc-200 p-6 rounded-full border border-white dark:border-zinc-900 absolute -top-10 left-1/2 -translate-x-1/2 shadow-xl">
               <Clock className="" strokeWidth={1.5} size={25} />
@@ -174,7 +168,7 @@ const WaitlistContent = () => {
               </svg>
             </div>
             <h2 className="text-2xl font-bold text-zinc-900 dark:text-white mb-2">
-              You're on the list!
+              You&apos;re on the list!
             </h2>
             <p className="text-zinc-600 dark:text-zinc-400 mb-6 font-medium">
               Thank you for joining. We'll be in touch soon with your early
--- src/components/WaitlistHeader.tsx
+++ src/components/WaitlistHeader.tsx
@@ -1,7 +1,8 @@
 import { useTheme } from "next-themes";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Twitter } from "lucide-react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
 const WaitlistHeader = () => {
   const { setTheme, resolvedTheme } = useTheme();
@@ -9,13 +10,19 @@ const WaitlistHeader = () => {
 
   useEffect(() => {
     setMounted(true);
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   if (!mounted) {
     return (
       <header className="mt-10 flex items-center justify-between bg-zinc-900/60 dark:bg-zinc-900/60 backdrop-blur-xl border border-zinc-900 dark:border-zinc-900 w-full rounded-full p-1 shadow-lg shadow-zinc-900/60">
         <div className="rounded-full">
-          <Image src={"/whop-logo.jpeg"} alt="whop-logo" width={40} height={40} className="rounded-full" />
+          <Image
+            src={"/whop-logo.jpeg"}
+            alt="whop-logo"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
         </div>
         <div className="p-3 bg-zinc-700/20 rounded-full w-10 h-10" />
       </header>
@@ -25,24 +32,36 @@ const WaitlistHeader = () => {
   const isDark = resolvedTheme === "dark";
 
   return (
-    <header className="mt-10 flex items-center justify-between bg-white/60 dark:bg-zinc-900/60 backdrop-blur-xl border border-zinc-200 dark:border-zinc-900 rounded-full max-w-sm mx-auto p-1 shadow-lg shadow-black/5 dark:shadow-zinc-900/50">
+    <header className="mt-10 flex items-center justify-between rounded-full max-w-full mx-4 md:mx-20 p-1">
       <div className="rounded-full">
-        <Image src={"/whop-logo.jpeg"} alt="whop-logo" width={40} height={40} className="rounded-full" />
+        <Image
+          src={"/whop-logo.jpeg"}
+          alt="whop-logo"
+          width={60}
+          height={60}
+          className="rounded-full"
+        />
       </div>
-      {/* theme changing container */}
-      <button
-        onClick={() => setTheme(isDark ? "light" : "dark")}
-        className="p-3 bg-zinc-100 dark:bg-zinc-700/20 hover:bg-zinc-200 dark:hover:bg-zinc-700/40 transition-colors rounded-full cursor-pointer"
-        aria-label="Toggle Theme"
-      >
-        <div className="text-zinc-900 dark:text-white">
-          {isDark ? (
-            <Sun size={18} strokeWidth={1.5} fill="currentColor" />
-          ) : (
-            <Moon size={18} strokeWidth={1.5} fill="currentColor" />
-          )}
-        </div>
-      </button>
+      <div className="flex space-x-2">
+        <Link href="/" className="flex items-center gap-2 px-4 md:px-6 py-1.5 transition-colors rounded-full text-black dark:text-white border-zinc-600 hover:border-zinc-300 dark:hover:border-zinc-200 dark:border-zinc-400 border">
+          <p className="hidden sm:block">Check us out on </p>
+          <Twitter size={20} strokeWidth={1.4} />
+        </Link>
+        {/* theme changing container */}
+        <button
+          onClick={() => setTheme(isDark ? "light" : "dark")}
+          className="p-3 bg-zinc-100 dark:bg-zinc-700/20 hover:bg-zinc-200 dark:hover:bg-zinc-700/40 transition-colors rounded-full cursor-pointer"
+          aria-label="Toggle Theme"
+        >
+          <div className="text-zinc-900 dark:text-white">
+            {isDark ? (
+              <Sun size={18} strokeWidth={1.5} fill="currentColor" />
+            ) : (
+              <Moon size={18} strokeWidth={1.5} fill="currentColor" />
+            )}
+          </div>
+        </button>
+      </div>
     </header>
   );
 };
